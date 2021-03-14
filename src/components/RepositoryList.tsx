91e8a0da5fbea4e8c39f67789a958a7bdf848332
Rafael Ramos <rafaelramos0401@gmail.com>
2021-03-14 15:43:24 -0300

    Refatorando a aplicação com typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,11 +3,17 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 // https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
 
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch(' https://api.github.com/orgs/rocketseat/repos')
